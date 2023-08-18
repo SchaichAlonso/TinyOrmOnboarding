07f49a96a51e529d03dc29ec7f1ed7be5dd456bb
Alonso Schaich <alonsoschaich@fastmail.fm>
2023-08-18 23:26:52 +0900

    make msbuild more verbose

--- build.ps1
+++ build.ps1
@@ -10,6 +10,10 @@ if (${env:VCPKG_TARGET_TRIPLET} -eq ${null}) {
   ${env:VCPKG_TARGET_TRIPLET}="x64-windows"
 }
 
+if ("x64-windows", "x64-windows-static" -contains "${env:VCPKG_TARGET_TRIPLET}") {
+  $cmake_build_args="-v:n"
+}
+
 cmake `
 	-B ${env:GITHUB_WORKSPACE}/build `
 	-DVCPKG_VERBOSE=ON `
@@ -18,4 +22,4 @@ cmake `
 	-DCMAKE_TOOLCHAIN_FILE="${env:GITHUB_WORKSPACE}/vcpkg/scripts/buildsystems/vcpkg.cmake" `
 	${env:GITHUB_WORKSPACE}
 
-cmake --build ${env:GITHUB_WORKSPACE}/build --config ${env:BUILD_TYPE}
+cmake --build ${env:GITHUB_WORKSPACE}/build --config ${env:BUILD_TYPE} -- ${cmake_build_args}
