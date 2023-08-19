dcd3413761e17df274030171724dc741b7fe9dd5
Schaich Alonso <alonsoschaich@fastmail.fm>
2023-08-19 12:55:00 +0000

    Fix static compilation

--- test.ps1
+++ test.ps1
@@ -10,12 +10,21 @@ if (${env:VCPKG_TARGET_TRIPLET} -eq ${null}) {
   ${env:VCPKG_TARGET_TRIPLET}="x64-windows"
 }
 
-$vcpkgPrefix="${env:GITHUB_WORKSPACE}/build/vcpkg_installed/${env:VCPKG_TARGET_TRIPLET}/"
-if ($env:BUILD_TYPE -eq "Debug") {
-	$vcpkgPrefix="${vcpkgPrefix}/debug"
+if ("x64-windows-static" -eq "${env:VCPKG_TARGET_TRIPLET}") {
+  ${msvc_runtime_dynamic}="Off"
+} else {
+  ${msvc_runtime_dynamic}="On"
 }
 
-${env:PATH}="${vcpkgPrefix}/bin;${env:PATH}"
-${env:QT_PLUGIN_PATH}="${vcpkgPrefix}/Qt6/plugins"
+$vcpkgPrefix="${env:GITHUB_WORKSPACE}/build/vcpkg_installed/${env:VCPKG_TARGET_TRIPLET}/"
+if ($env:BUILD_TYPE -eq "Debug") {
+  ${vcpkgPrefix}="${vcpkgPrefix}/debug"
+}
+
+if (${msvc_runtime_dynamic} -eq "On") {
+  ${env:PATH}="${vcpkgPrefix}/bin;${env:PATH}"
+  ${env:QT_PLUGIN_PATH}="${vcpkgPrefix}/Qt6/plugins"
+}
+
 cd "${env:GITHUB_WORKSPACE}/build"
 ctest --test-dir "${env:GITHUB_WORKSPACE}/build" -C "${env:BUILD_TYPE}" --output-junit "${env:GITHUB_WORKSPACE}/build/ctest.xml"
