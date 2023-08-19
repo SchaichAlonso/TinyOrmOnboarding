89eec7e84d8a721deff32ff552050df27c36fd20
Schaich, Alonso <alonsoschaich@fastmail.fm>
2023-08-19 16:17:39 +0900

    Add QSqlQuery raw testcase

--- test.ps1
+++ test.ps1
@@ -18,5 +18,4 @@ if ($env:BUILD_TYPE -eq "Debug") {
 ${env:PATH}="${vcpkgPrefix}/bin;${env:PATH}"
 ${env:QT_PLUGIN_PATH}="${vcpkgPrefix}/Qt6/plugins"
 cd "${env:GITHUB_WORKSPACE}/build"
-& "${env:GITHUB_WORKSPACE}/build/${env:BUILD_TYPE}/MyTest" -junitxml > "${env:GITHUB_WORKSPACE}/build/result.xml"
-#ctest --test-dir "${env:GITHUB_WORKSPACE}/build" -C "${env:BUILD_TYPE}" --output-junit "${env:GITHUB_WORKSPACE}/build/result.xml"
+ctest --test-dir "${env:GITHUB_WORKSPACE}/build" -C "${env:BUILD_TYPE}" --output-junit "${env:GITHUB_WORKSPACE}/build/ctest.xml"
