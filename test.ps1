c838fcf9c9b06c10625489f99c1d1721138c2f55
Alonso Schaich <alonsoschaich@fastmail.fm>
2023-08-20 05:50:23 +0900

    Use GitHub Action Matrix (#5)
    * Use Matrix
    
    * Update vcpkg submodule to tip of branch
    
    * Use provided vcpkg target triplet
    
    * Use provided target triplet
    
    * Provide vcpkg target triplet
    
    * Disable matrix.fail-fast
    
    * Update vcpkg
    
    * make msbuild more verbose
    
    * Install ubuntu dependencies
    
    * Consume Qt::Sql
    
    * Try forcing Qt::Sql plugin inclusion
    
    * Add QSqlQuery raw testcase
    
    * Fix static compilation
    
    * Update vcpkg
    
    * Update vcpkg submodule after rebase of it
    
    * matrixify BUILD_TYPE
    
    * Increase build verbosity

--- test.ps1
+++ test.ps1
@@ -6,13 +6,25 @@ if (${env:BUILD_TYPE} -eq ${null}) {
   ${env:BUILD_TYPE}="debug"
 }
 
-$vcpkgPrefix="${env:GITHUB_WORKSPACE}/build/vcpkg_installed/x64-windows/"
-if ($env:BUILD_TYPE -eq "Debug") {
-	$vcpkgPrefix="${vcpkgPrefix}/debug"
+if (${env:VCPKG_TARGET_TRIPLET} -eq ${null}) {
+  ${env:VCPKG_TARGET_TRIPLET}="x64-windows"
 }
 
-${env:PATH}="${vcpkgPrefix}/bin;${env:PATH}"
-${env:QT_PLUGIN_PATH}="${vcpkgPrefix}/Qt6/plugins"
+if ("x64-windows-static" -eq "${env:VCPKG_TARGET_TRIPLET}") {
+  ${msvc_runtime_dynamic}="Off"
+} else {
+  ${msvc_runtime_dynamic}="On"
+}
+
+$vcpkgPrefix="${env:GITHUB_WORKSPACE}/build/vcpkg_installed/${env:VCPKG_TARGET_TRIPLET}/"
+if ($env:BUILD_TYPE -eq "Debug") {
+  ${vcpkgPrefix}="${vcpkgPrefix}/debug"
+}
+
+if (${msvc_runtime_dynamic} -eq "On") {
+  ${env:PATH}="${vcpkgPrefix}/bin;${env:PATH}"
+  ${env:QT_PLUGIN_PATH}="${vcpkgPrefix}/Qt6/plugins"
+}
+
 cd "${env:GITHUB_WORKSPACE}/build"
-& "${env:GITHUB_WORKSPACE}/build/${env:BUILD_TYPE}/MyTest" -junitxml > "${env:GITHUB_WORKSPACE}/build/result.xml"
-#ctest --test-dir "${env:GITHUB_WORKSPACE}/build" -C "${env:BUILD_TYPE}" --output-junit "${env:GITHUB_WORKSPACE}/build/result.xml"
+ctest --test-dir "${env:GITHUB_WORKSPACE}/build" -C "${env:BUILD_TYPE}" --output-junit "${env:GITHUB_WORKSPACE}/build/ctest.xml"
